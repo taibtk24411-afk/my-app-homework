c2ca4f366566c09dfb583ec492d3d1aa35964354
Tan Tai Bui <taibtk24411@st.uel.edu.vn>
2026-09-24 17:36:30 +0700

    xog

--- src/app/bai18/customer.spec.ts
+++ src/app/bai18/customer.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
-import { Customer } from './customer';
+import { CustomerService } from './customer';
 
-describe('Customer', () => {
-  let service: Customer;
+describe('CustomerService', () => {
+  let service: CustomerService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(Customer);
+    service = TestBed.inject(CustomerService);
   });
 
   it('should be created', () => {
